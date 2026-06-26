ede7b269bb51178615b3a071d86e46b0595ae90e
amirmuratov007 <amirmuratov007@gmail.com>
2026-06-26 15:55:59 +0300

    Refresh offline cache for rebuilt console

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,16 @@
-const CACHE_NAME = "heimdall-family-protection-v9";
-const ASSETS = ["./", "./index.html", "./en.html", "./ru.html", "./style.css", "./network.js", "./risk.js", "./manifest.webmanifest", "./heimdall-logo.jpg", "./heimdall-logo.svg", "./shield-icon.svg"];
+const CACHE_NAME = "heimdall-family-protection-v10";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./en.html",
+  "./ru.html",
+  "./style.css",
+  "./network.js",
+  "./manifest.webmanifest",
+  "./heimdall-logo.jpg",
+  "./heimdall-logo.svg",
+  "./shield-icon.svg"
+];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
@@ -8,18 +19,20 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
