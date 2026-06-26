cfa77c2d5403a894c8079456676fc728a895d636
amirmuratov007 <amirmuratov007@gmail.com>
2026-06-26 12:38:15 +0300

    Cache bilingual Heimdall pages

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "heimdall-family-protection-v1";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./shield-icon.svg"];
+const CACHE_NAME = "heimdall-family-protection-v2";
+const ASSETS = ["./", "./index.html", "./en.html", "./ru.html", "./style.css", "./network.js", "./risk.js", "./manifest.webmanifest", "./heimdall-logo.svg", "./shield-icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
